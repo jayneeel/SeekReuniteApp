e52df65239281596a3bf9077359e389d5883c2ce
AdityaBhopalbade <adityarocks996@gmail.com>
2024-02-12 23:33:57 +0530

    profile screen update 2

--- lib/presentation/profile/profile_screen.dart
+++ lib/presentation/profile/profile_screen.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
 
-import '../../main.dart';
-
-void main() {
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.dark
-  ));
-  runApp(const MyApp());
-}
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
